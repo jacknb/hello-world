516f3743eba72a05e5fd35867a8270a1c89f7853
jacknb <zhuningbin@sina.com>
2018-01-13 23:28:55 +0800

    练习

---
+++ src/main/java/com/adelmo/kafka/KafkaProducer.java
@@ -0,0 +1,14 @@
+package com.adelmo.kafka;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by znb on 17-10-15.
+ */
+public class KafkaProducer {
+
+    public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+    }
+
+}
